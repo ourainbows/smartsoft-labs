0005356ff7c65ae676546404be396729be7dcbfd
ourainbows <casasjuanfelipe@gmail.com>
2022-06-13 02:39:11 -0500

    Logig for dialog component added

--- src/app/home/products/dialog/dialog.component.ts
+++ src/app/home/products/dialog/dialog.component.ts
@@ -1,15 +1,21 @@
+import { DialogContentComponent } from './../dialog-content/dialog-content.component';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent{
+ constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentComponent);
 
-  ngOnInit(): void {
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
