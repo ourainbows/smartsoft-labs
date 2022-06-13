d839ff8f03775e85bee3c92e106064a30ae20da9
ourainbows <casasjuanfelipe@gmail.com>
2022-06-13 02:31:51 -0500

    Generated dialgo and dialog-content component

--- src/app/home/products/products.module.ts
+++ src/app/home/products/products.module.ts
@@ -6,10 +6,12 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DialogComponent } from './dialog/dialog.component';
+import { DialogContentComponent } from './dialog-content/dialog-content.component';
 
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, DialogComponent, DialogContentComponent],
   imports: [CommonModule, ProductsRoutingModule, MatTableModule,MatPaginatorModule, ReactiveFormsModule],
 })
 export class ProductsModule {}
